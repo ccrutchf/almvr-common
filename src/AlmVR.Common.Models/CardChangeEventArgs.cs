db9b60f24e09c342aff92bffef882f270879e874
Chris Crutchfield <ccrutchf@ucsd.edu>
2018-09-08 14:13:28 -0700

    Card changed event args

---
+++ src/AlmVR.Common.Models/CardChangeEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlmVR.Common.Models
+{
+    public class CardChangedEventArgs : EventArgs
+    {
+        public CardModel Card { get; private set; }
+
+        public CardChangedEventArgs(CardModel card)
+        {
+            Card = card;
+        }
+    }
+}
